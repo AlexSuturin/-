0aa34ff19ba13470417e0484fbde9161fb16bc42
AlexSuturin <164014384+AlexSuturin@users.noreply.github.com>
2024-05-05 22:41:16 +0300

    ordersTable.js

--- ExamProject/js/ordersTable.js
+++ ExamProject/js/ordersTable.js
@@ -9,7 +9,6 @@ const itemsPerPage = 5
 let currentPage = 1
 let i = 1
 
-// id текущего заказа ()
 let currentOrderId = null
 
 const API_KEY = '9c1137b1-0cc9-4f15-9def-fb82f3a67bcc'
@@ -20,20 +19,15 @@ function fetchOrders () {
   fetch(`http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/orders?api_key=${API_KEY}`)
     .then(response => response.json())
     .then(data => {
-			// сортировка заказов по убыванию id
       data.sort((a, b) => b.id - a.id)
 
-			// вывод заказов на страницу
       displayOrders(data)
 
 // пагинация
-// количество страниц
       const totalPages = Math.ceil(data.length / itemsPerPage)
-			// контейнер для пагинации
       const paginationContainer = document.createElement('nav')
       paginationContainer.setAttribute('aria-label', '...')
 
-			// создание списка для пагинации
       const paginationList = document.createElement('ul')
       paginationList.classList.add('pagination')
 
@@ -71,7 +65,6 @@ function fetchOrders () {
         pageLink.href = '#routes'
         pageLink.innerText = i
 
-				// подсветка текущей страницы
         if (i === currentPage) {
           pageItem.classList.add('active')
         }
@@ -123,17 +116,12 @@ function fetchOrders () {
 
 // функция для вывода заказов на страницу
 function displayOrders (data) {
-	// начальный и конечный индексы для пагинации
   const startIndex = (currentPage - 1) * itemsPerPage
   const endIndex = startIndex + itemsPerPage
-	// данные для текущей страницы
   const paginatedData = data.slice(startIndex, endIndex)
 
-	// индекс для нумерации заказов
   i = startIndex + 1
 
-	// если заказов нет
-	// выводим сообщение "Нет заказов"
   if (paginatedData.length === 0) {
     ordersTable.innerHTML = `
     <thead class="table-primary">
@@ -152,8 +140,6 @@ function displayOrders (data) {
     return
   }
 
-	// вывод заказов на страницу
-	// вывод сначала заголовков таблицы
   ordersTable.innerHTML = `
     <thead class="table-primary">
       <td class="fw-bold" style="background: #c76a30 !important; color: white;border-radius: 10px 0 0 0">№</td>
@@ -164,7 +150,6 @@ function displayOrders (data) {
     </thead>
     `
 
-		// затем для каждого заказа добавляется строка в таблицу
   const routeIds = new Set()
   data.forEach(item => {
     routeIds.add(item.route_id)
@@ -172,24 +157,18 @@ function displayOrders (data) {
 
 	// объект для хранения названий маршрутов
   const routeNames = {}
-	// запросы для получения названий маршрутов
   const requests = Array.from(routeIds).map(id =>
     fetch('http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/routes/' + id + '?api_key=' + API_KEY)
       .then(response => response.json())
       .then(data => {
-				// сохранение названий маршрутов в объект
         routeNames[id] = data.name
       })
       .catch(e => console.log(e))
   )
 
-	// ожидание завершения всех запросов
   Promise.all(requests)
-	// вывод заказов на страницу
     .then(() => {
-			// вывод заказов на страницу (строки таблицы)
       paginatedData.forEach(item => {
-				//выводится информация во 2 столбце, а в 3 столбце - кнопки для просмотра, редактирования и удаления заказа
         ordersTable.innerHTML += `
           <tr>
             <td>${item.id}</td>
